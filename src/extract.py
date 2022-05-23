282e307789173bd6fadeae38d8741041003a5bad
Jonathan Henrique <jhowjhoe@icloud.com>
2022-05-22 22:04:52 -0300

    updates no crash script

--- src/extract.py
+++ src/extract.py
@@ -11,11 +11,18 @@ f = open("contatos.txt", "a+")
 
 options = Options()
 options.headless = True # False
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
-options.add_argument('--disable-gpu')
-options.add_argument('--window-size=1920x1080')
-options.accept_insecure_certs = True
+
+options.add_argument("--window-size=1920,1080")
+options.add_argument("--no-sandbox")
+options.add_argument("--headless")
+options.add_argument("--disable-gpu")
+options.add_argument("--disable-crash-reporter")
+options.add_argument("--disable-extensions")
+options.add_argument("--disable-in-process-stack-traces")
+options.add_argument("--disable-logging")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--log-level=3")
+options.add_argument("--output=/dev/null")
 
 driver = webdriver.Chrome(options=options, executable_path=DRIVER_PATH)
 #driver.get("https://mg.olx.com.br/regiao-de-uberlandia-e-uberaba/autos-e-pecas/carros-vans-e-utilitarios/loucura-total-imperdivel-trailblazer-2014-automatica-muito-nova-agio-so-62-mil-reais-1005300647#")
@@ -23,20 +30,35 @@ driver = webdriver.Chrome(options=options, executable_path=DRIVER_PATH)
 with open(filename) as fp:
     urls_anuncios = json.load(fp)
 
-#loop interate through the list
 for url_anuncio in urls_anuncios:
-    print(url_anuncio.get("anuncio"))
+
+    print(url_anuncio.get("anuncio")+'\n')
+    print('--------------------\n')
 
     driver.get(url_anuncio.get("anuncio"))
 
-    element = driver.find_element_by_id("initial-data")
-    json_text = json.loads(element.get_attribute('data-json'))
+    if(driver.title != 'Anúncio não encontrado | OLX'):
 
-    ad = json_text.get('ad')
-    user = ad.get('user')
-    phone = ad.get('phone')
+        element = driver.find_element_by_id("initial-data")
+        if( element is not None):
+            json_text = json.loads(element.get_attribute('data-json'))
 
-    f = open("contatos.txt", "a+")
-    f.write('Nome: ' + str(user.get('name')) + "\n" + 'Descricao: ' + str(ad.get('body')) + "\n" + "Telefone: " + str(phone.get('phone')) + "\n" + "Verificado: " + str(phone.get('phoneVerified')) + "\n" + "Anuncio: " + str(url_anuncio.get('anuncio')) + "\n" + "\n")
+            ad = json_text.get('ad')
 
-    time.sleep(15)
+            if(ad is not None):
+                user = ad.get('user')
+                phone = ad.get('phone')
+
+                f = open("contatos.txt", "a+")
+                f.write('Nome: ' + str(user.get('name')) + "\n" + 'Descricao: ' + str(ad.get('body')) + "\n" + "Telefone: " + str(phone.get('phone')) + "\n" + "Verificado: " + str(phone.get('phoneVerified')) + "\n" + "Anuncio: " + str(url_anuncio.get('anuncio')) + "\n" + "\n")
+            else:
+                print("Não encontrado")
+                f = open("contatos.txt", "a+")
+                f.write('Anuncio vazio: ' + str(url_anuncio.get('anuncio')) + "\n" + "\n")
+
+    else:
+        print('Anuncio nao encontrado')
+        f = open("contatos.txt", "a+")
+        f.write('Anuncio nao encontrado: ' + str(url_anuncio.get('anuncio')) + "\n" + "\n")
+
+time.sleep(25)
--- src/scrap.py
+++ src/scrap.py
@@ -1,53 +1,64 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import json
+import time 
 
 DRIVER_PATH = './chrome/chromedriver.exe'
 
-#f = open("./urls.json","w+")
 filename = "./urls.json"
 listObj = []
 
 options = Options()
 options.headless = True # False
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
-options.add_argument('--disable-gpu')
-options.add_argument('--window-size=1920x1080')
 options.accept_insecure_certs = True
 
+options.add_argument("--window-size=1920,1080")
+options.add_argument("--no-sandbox")
+options.add_argument("--headless")
+options.add_argument("--disable-gpu")
+options.add_argument("--disable-crash-reporter")
+options.add_argument("--disable-extensions")
+options.add_argument("--disable-in-process-stack-traces")
+options.add_argument("--disable-logging")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--log-level=3")
+options.add_argument("--output=/dev/null")
+
 driver = webdriver.Chrome(options=options, executable_path=DRIVER_PATH)
-driver.get("https://mg.olx.com.br/regiao-de-uberlandia-e-uberaba/autos-e-pecas/carros-vans-e-utilitarios?f=p")
 
-links = driver.find_elements_by_tag_name("a")
+for route in list(range(1,100)):
 
-for link in links:
+    print('\nLink line: ' + str(route)+'\nRoute Url: https://mg.olx.com.br/regiao-de-uberlandia-e-uberaba/autos-e-pecas/carros-vans-e-utilitarios?f=p&o='+str(route)+'\n')
+    print('----------------------\n')
 
-    if link.get_attribute("href") is not None:
-        url = link.get_attribute("href")
+    driver.get("https://mg.olx.com.br/regiao-de-uberlandia-e-uberaba/autos-e-pecas/carros-vans-e-utilitarios?f=p&o="+str(route))
 
-        anuncio = url.split("/")
-        size = len(anuncio)
+    links = driver.find_elements_by_tag_name("a")
 
-        if size >= 6:
-            if anuncio[5] == "carros-vans-e-utilitarios":
-                #print(url)
-                #f.write("["+url+"]"+",")
-                # Read JSON file
-                with open(filename) as fp:
-                    listObj = json.load(fp)
-                
-                # Verify existing list
-                print(listObj)
+    for link in links:
 
-                print(type(listObj))
-                
-                listObj.append({
-                    "anuncio": url,
-                })
-                
-                # Verify updated list
-                print(listObj)
-                
-                with open(filename, 'w') as json_file:
-                    json.dump(listObj, json_file, indent=4, separators=(',',': '))
+        if link.get_attribute("href") is not None:
+            url = link.get_attribute("href")
+
+            anuncio = url.split("/")
+            size = len(anuncio)
+
+            if size >= 6:
+                if anuncio[5] == "carros-vans-e-utilitarios":
+                    with open(filename) as fp:
+                        listObj = json.load(fp)
+                    
+                    listObj.append({
+                        "page": str(route),
+                        "anuncio": url,
+                    })
+                    
+                    with open(filename, 'w') as json_file:
+                        json.dump(listObj, json_file, indent=4, separators=(',',': '))
+        else:
+            print('\nLink: ' + str(link)+'\n')
+            print('----------------------\n')
+
+    time.sleep(15)
+    
+time.sleep(5)
